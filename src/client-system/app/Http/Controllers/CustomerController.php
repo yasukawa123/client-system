75d8d62b7d9b4850bf7f479a3442239a4170d360
yasukawa123 <kazu050107@gmail.com>
2022-05-17 15:56:14 +0900

    顧客一覧の表示

---
+++ src/client-system/app/Http/Controllers/CustomerController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // 顧客情報はスーパーバイザーであれば 全店舗の顧客情報を閲覧できますが、店員の場合は自分が所属する店舗の顧客情報しか閲覧できません。
+        if (auth()->user()->isSuperVisor()) {
+            $customers = Customer::paginate();
+        } else {
+            $customers = Customer::where('shop_id', auth()->user()['shop_id'])->paginate();
+        }
+        return view('customers.index', compact('customers'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Customer $customer)
+    {
+        //
+    }
+}
--- src/client-system/routes/web.php
+++ src/client-system/routes/web.php
@@ -13,17 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::get('/test', function () {
-    return view('hello');
-});
-
-Route::get('page_a', function () {
-    return view('page_a');
-});
-Route::get('page_b', function () {
-    return view('page_b');
-});
+// Viewsテスト用
+// Route::get('/test', function () {
+//     return view('hello');
+// });
+// Route::get('page_a', function () {
+//     return view('page_a');
+// });
+// Route::get('page_b', function () {
+//     return view('page_b');
+// });
 
 Route::get('/', [\App\Http\Controllers\TopPageController::class, 'top_page'])->name('top_page');
 
@@ -34,4 +33,6 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 // 社員一覧呼び出し
 Route::get('/users', \App\Http\Controllers\UserController::class)->name('社員一覧')->middleware('auth');
 // 役職一覧呼び出し
-Route::get('/roles', \App\Http\Controllers\RoleController::class)->name('役職一覧')->middleware('auth');
+Route::get('/roles', \App\Http\Controllers\RoleController::class)->name('ロール一覧')->middleware('auth');
+// 顧客管理一覧
+Route::resource('/customers', \App\Http\Controllers\CustomerController::class)->middleware('auth');
