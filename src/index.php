e40d6a804afe54d275affb211ef8e936f11989f5
yasukawa123 <kazu050107@gmail.com>
2022-06-20 13:36:36 +0900

    index.php

--- src/index.php
+++ src/index.php
@@ -1,16 +1,1 @@
-<?php
-	
-	echo 'Docker Local';
-	
-	$dsn = 'mysql:host='.$_ENV['MYSQL_HOST']
-	     .';dbname='.$_ENV['MYSQL_DATABASE'];
-	
-	$dbh = new PDO($dsn, $_ENV['MYSQL_USER'], $_ENV['MYSQL_PASSWORD']);
-	
-	foreach($dbh->query('SHOW databases') as $row) {
-	    echo '<pre>';
-	    print_r($row);
-	    echo '</pre>';
-	}
-	
-phpinfo();
+<?php include_once("index.html"); ?>
