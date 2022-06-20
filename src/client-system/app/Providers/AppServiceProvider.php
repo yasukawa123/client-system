09eeb39aed246c1dddfe3fac8003034dd30cac2e
yasukawa123 <kazu050107@gmail.com>
2022-06-20 13:34:50 +0900

    composer.json

--- src/client-system/app/Providers/AppServiceProvider.php
+++ src/client-system/app/Providers/AppServiceProvider.php
@@ -26,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
         // 以下を追記
         if (\App::environment('production')) {
             \URL::forceScheme('https');
+        }
     }
     
 }
